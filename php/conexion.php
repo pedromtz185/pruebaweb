fd68c549a5712b8e7f81627641d6c81ae282fe58
PedroMtzS185 <46005104+pedromtz185@users.noreply.github.com>
2019-07-30 08:16:48 -0500

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -2,7 +2,7 @@
 		function conexion(){
 			$servidor="localhost";
 			$usuario="root";
-			$password="root1234";
+			$password="root";
 			$bd="pruebaweb";
 			$conexion=mysqli_connect($servidor,$usuario,$password,$bd);
 			return $conexion;
